328e3217a2c3f6fce8669fb936a44e04c49305f7
watchtheblind <ljmm2k2@gmail.com>
2026-06-27 10:48:32 -0400

    feat: install and use globally  nestjs/config

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // disponible en toda la app sin importar cada vez
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
